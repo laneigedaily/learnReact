a3d431d767f2378518e8cb923bf8bd3d52886fdc
laneigedaily <87255831+laneigedaily@users.noreply.github.com>
2024-11-22 13:29:49 +0900

    prefetch

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,20 @@ import { Suspense } from "react";
 import MovieInfo, { getMovies } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export async function generateMetadata({ params: { id } }) {
+type IParams = Promise<{ id: string }>;
+
+export async function generateMetadata(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
   const movie = await getMovies(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default async function MovieDetailPage(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
